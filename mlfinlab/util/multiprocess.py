fb3ec0952f28e661cf9158f460d4b386a503f47b
mehdi63amini <mehdi63amini@gmail.com>
2024-10-03 21:13:04 +0330

    Update multiprocess.py

--- mlfinlab/util/multiprocess.py
+++ mlfinlab/util/multiprocess.py
@@ -134,7 +134,7 @@ def mp_pandas_obj(func, pd_obj, num_threads=24, mp_batches=1, lin_mols=True, ver
         return out
 
     for i in out:
-        df0 = df0.append(i)
+        df0 = df0._append(i)
 
     df0 = df0.sort_index()
     return df0
